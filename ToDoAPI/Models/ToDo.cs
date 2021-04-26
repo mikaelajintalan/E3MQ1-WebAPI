c384f6b69d09d5aed176bc85496d78c62b292fe4
mikaelajintalan <mikaela.jintalan@gmail.com>
2021-04-26 12:24:44 +0800

    Add project files.

---
+++ ToDoAPI/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAPI.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int ToDoId { get; set; }
+
+        [Column(TypeName ="nvarchar=(100)")]
+        public string ToDoName { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ ToDoAPI/Models/ToDoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAPI.Models
+{
+    public class ToDoDbContext:DbContext
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
